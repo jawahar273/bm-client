a2eaee04b37f91fee0980c93a6fcf6e12d9da6bd
jawahar273 <jawahar273@gmail.com>
2018-01-30 17:03:23 +0530

    changs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,6 +46,9 @@ export function createTranslateLoader(http: HttpClient) {
         }),
         AppRoutingModule,
         NgbModule.forRoot(),
+        // if you add diffrent base href pls update with relative path at font..
+        // this might be bug and has been reposted 
+        // https://github.com/angular/angular-cli/issues/9406 and https://github.com/angular/angular-cli/issues/8515
         environment.production ? ServiceWorkerModule.register('/bm-client/ngsw-worker.js') : [],
         AsyncLocalStorageModule,
     ],
