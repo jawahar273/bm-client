e3a8c89b5466c439cd18edaae3776663cd61cb6d
jawahar273 <jawahar273@gmail.com>
2018-02-01 22:08:16 +0530

    change in gitlab yml

--- protractor.conf.js
+++ protractor.conf.js
@@ -10,9 +10,9 @@ exports.config = {
   ],
   capabilities: {
     'browserName': 'chrome'
-    // chromeOptions: {
-    //  args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
-    // }
+    chromeOptions: {
+     args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4400/',
