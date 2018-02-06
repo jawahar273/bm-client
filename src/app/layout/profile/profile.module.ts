b44ea8d25c6bc1dfac550cf64f9e6a0bfd947855
jawahar273 <jawahar273@gmail.com>
2018-02-06 11:58:38 +0530

    correcting build error @ci no#17115325

--- src/app/layout/profile/profile.module.ts
+++ src/app/layout/profile/profile.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
+import { ProfileComponent } from './profile.component';
 
 @NgModule({
   imports: [
